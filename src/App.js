632344d083b36b1d98adfa87f189ac1381920ac9
JoesphAguilar <josephjd93@gmail.com>
2022-03-22 13:50:10 -0700

    set up likePost function; debugging likes not working

--- src/App.js
+++ src/App.js
@@ -34,7 +34,14 @@ const App = () => {
         - otherwise just return the post object unchanged.
      */
 
-        
+    const updatePosts = posts.map(post => {
+      if(post.id === postId) {
+        return {...post, likes: post.likes + 1}
+      } else {
+        return post;
+      }
+    })
+    setPosts(updatePosts);
   };
 
   return (
